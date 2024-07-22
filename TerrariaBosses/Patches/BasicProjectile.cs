3eae729a624da82d525d8122ac4a7b2b4fc6e236
Glitch <glitchedgameronyt@gmail.com>
2024-07-22 09:07:30 -0700

    Squashed commit of the following:
    commit 9a245673c97b81bc840a85d54aee61d652abba70
    Author: Glitch <glitchedgameronyt@gmail.com>
    Date:   Mon Jul 22 09:03:12 2024 -0700
    
        Modify Pull Request
    
        Reverted changes to names
        Removed AssetHelper.cs
        Reformatted CP mod
        Removed unnecessary assets
        Reverted second phase health back to 50%
        Fixed Animation
    
    commit 580c8cff7270194a1a40a5215ad8ea52ae38b9b1
    Author: Ryan <redryma@icloud.com>
    Date:   Mon Jul 15 02:13:46 2024 -0400
    
        Work on fixing some issues with the Eye of Cthulhu and put LICENSE in root
    
    commit 0c089ee38bc9b129424a35fbbb6f498e21ba705a
    Author: Ryan <redryma@icloud.com>
    Date:   Sun Jul 14 23:30:42 2024 -0400
    
        Fix monsters from not spawning
    
    commit 4a14f8edd57ab510b3f5874bbb773b5003717d54
    Author: Ryan <redryma@icloud.com>
    Date:   Sun Jul 14 22:43:36 2024 -0400
    
        remove bin folder and add to .gitignore
    
    commit f47a6f1b0a86e43ef0060bd7afd4c98ab6e1cd18
    Author: Ryan <redryma@icloud.com>
    Date:   Sun Jul 14 22:40:57 2024 -0400
    
        Change loading assets to be handled by content patcher

---
+++ TerrariaBosses/Patches/BasicProjectile.cs
@@ -0,0 +1,55 @@
+﻿using StardewValley;
+using Microsoft.Xna.Framework;
+using StardewValley.Extensions;
+
+namespace TerrariaBosses.Patches
+{
+    internal class BasicProjectile
+    {
+        private static void createRadialDebris(GameLocation location, string texture, Rectangle sourcerectangle, int xTile, int yTile, int numberOfChunks)
+        {
+            Vector2 debrisOrigin = new Vector2(xTile * 64 + 64, yTile * 64 + 64);
+            location.debris.Add(new SlingshotDebris(texture, sourcerectangle, numberOfChunks / 4, debrisOrigin, debrisOrigin + new Vector2(-64f, 0f)));
+            numberOfChunks++;
+            location.debris.Add(new SlingshotDebris(texture, sourcerectangle, numberOfChunks / 4, debrisOrigin, debrisOrigin + new Vector2(64f, 0f)));
+            numberOfChunks++;
+            location.debris.Add(new SlingshotDebris(texture, sourcerectangle, numberOfChunks / 4, debrisOrigin, debrisOrigin + new Vector2(0f, -64f)));
+            numberOfChunks++;
+            location.debris.Add(new SlingshotDebris(texture, sourcerectangle, numberOfChunks / 4, debrisOrigin, debrisOrigin + new Vector2(0f, 64f)));
+        }
+        public static bool explosionAnimation_Prefix(StardewValley.Projectiles.BasicProjectile __instance, GameLocation location)
+        {
+            if (__instance.itemId.Value == "(O)GlitchedDeveloper.TerrariaBosses_CrimtaneOre")
+            {
+                createRadialDebris(location, "Mods\\GlitchedDeveloper.TerrariaBosses\\Debris\\Crimtane", new Rectangle(0, 0, 32, 16), (int)(__instance.position.X + 32f) / 64, (int)(__instance.position.Y + 32f) / 64, 6);
+                if (!string.IsNullOrEmpty(__instance.collisionSound.Value))
+                {
+                    location.playSound(__instance.collisionSound);
+                }
+                if ((bool)__instance.explode)
+                {
+                    Game1.Multiplayer.broadcastSprites(location, new TemporaryAnimatedSprite(362, Game1.random.Next(30, 90), 6, 1, __instance.position.Value, flicker: false, Game1.random.NextBool()));
+                }
+                __instance.collisionBehavior?.Invoke(location, __instance.getBoundingBox().Center.X, __instance.getBoundingBox().Center.Y, __instance.GetPlayerWhoFiredMe(location));
+                __instance.destroyMe = true;
+                return false;
+            }
+            else if (__instance.itemId.Value == "(O)GlitchedDeveloper.TerrariaBosses_DemoniteOre")
+            {
+                createRadialDebris(location, "Mods\\GlitchedDeveloper.TerrariaBosses\\Debris\\Demonite", new Rectangle(0, 0, 32, 16), (int)(__instance.position.X + 32f) / 64, (int)(__instance.position.Y + 32f) / 64, 6);
+                if (!string.IsNullOrEmpty(__instance.collisionSound.Value))
+                {
+                    location.playSound(__instance.collisionSound);
+                }
+                if ((bool)__instance.explode)
+                {
+                    Game1.Multiplayer.broadcastSprites(location, new TemporaryAnimatedSprite(362, Game1.random.Next(30, 90), 6, 1, __instance.position.Value, flicker: false, Game1.random.NextBool()));
+                }
+                __instance.collisionBehavior?.Invoke(location, __instance.getBoundingBox().Center.X, __instance.getBoundingBox().Center.Y, __instance.GetPlayerWhoFiredMe(location));
+                __instance.destroyMe = true;
+                return false;
+            }
+            return true;
+        }
+    }
+}
